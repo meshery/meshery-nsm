0f3f36b9a3d37316ed473fcf3fb176d766cc0cfb
dhruv0000 <patel.4@iitj.ac.in>
2021-07-14 23:56:42 +0530

    Migrated errors
    Signed-off-by: dhruv0000 <patel.4@iitj.ac.in>

--- nsm/error.go
+++ nsm/error.go
@@ -55,75 +55,63 @@ var (
 
 	// ErrOpInvalid represents the errors which are generated
 	// when an invalid operation is requested
-	ErrOpInvalid = errors.NewDefault(errors.ErrOpInvalid, "Invalid operation")
+	ErrOpInvalidCode = "nsm_test_code"
+
+	// ErrOpInvalid is an error when an invalid operation is requested
+	ErrOpInvalid = errors.New(ErrOpInvalidCode, errors.Alert, []string{"Invalid Operation"}, []string{"Opeartion is not supported by the adapter"}, []string{}, []string{})
 )
 
 // ErrInstallNSM is the error for install mesh
 func ErrInstallNSM(err error) error {
-	return errors.NewDefault(ErrInstallNSMCode, fmt.Sprintf("Error with nsm operation: %s", err.Error()))
+	return errors.New(ErrInstallNSMCode, errors.Alert, []string{"Error with nsm opeatation"}, []string{err.Error()}, []string{}, []string{})
 }
 
 // ErrCreatingHelmIndex is the error for creating helm index
 func ErrCreatingHelmIndex(err error) error {
-	return errors.NewDefault(ErrInstallNSMCode, fmt.Sprintf("Error with nsm operation: %s", err.Error()))
+	return errors.New(ErrCreatingHelmIndexCode, errors.Alert, []string{"Error while creating Helm Index"}, []string{err.Error()}, []string{}, []string{})
 }
 
 // ErrEntryWithAppVersionNotExists is the error when an entry with the given app version is not found
 func ErrEntryWithAppVersionNotExists(entry, appVersion string) error {
-	return errors.NewDefault(
-		ErrEntryWithAppVersionNotExistsCode,
-		fmt.Sprintf("entry %s with app version %s does not exists", entry, appVersion),
-	)
+	return errors.New(ErrEntryWithAppVersionNotExistsCode, errors.Alert, []string{"Entry for the app version does not exist"}, []string{fmt.Sprintf("entry %s with app version %s does not exists", entry, appVersion)}, []string{}, []string{})
 }
 
 // ErrHelmRepositoryNotFound is the error when no valid remote helm repository is found
 func ErrHelmRepositoryNotFound(repo string, err error) error {
-	return errors.NewDefault(
-		ErrHelmRepositoryNotFoundCode,
-		fmt.Sprintf("either the repo %s does not exists or is corrupt: %v", repo, err),
-	)
+	return errors.New(ErrHelmRepositoryNotFoundCode, errors.Alert, []string{"Helm repo not found"}, []string{fmt.Sprintf("either the repo %s does not exists or is corrupt: %v", repo, err)}, []string{}, []string{})
 }
 
 // ErrDecodeYaml is the error when the yaml unmarshal fails
 func ErrDecodeYaml(err error) error {
-	return errors.NewDefault(
-		ErrDecodeYamlCode,
-		fmt.Sprintf("error decoding yaml: %v", err),
-	)
+	return errors.New(ErrDecodeYamlCode, errors.Alert, []string{"Error occured while decoding YAML"}, []string{err.Error()}, []string{}, []string{})
 }
 
 // ErrMeshConfig is the error for mesh config
 func ErrMeshConfig(err error) error {
-	return errors.NewDefault(ErrMeshConfigCode, fmt.Sprintf("Error configuration mesh: %s", err.Error()))
+	return errors.New(ErrMeshConfigCode, errors.Alert, []string{"Error configuration mesh"}, []string{err.Error()}, []string{}, []string{})
 }
 
 // ErrApplyHelmChart is the error for applying helm chart
 func ErrApplyHelmChart(err error) error {
-	return errors.NewDefault(ErrApplyHelmChartCode, fmt.Sprintf("error applying helm chart: %s", err.Error()))
+	return errors.New(ErrApplyHelmChartCode, errors.Alert, []string{"Error occured while applying Helm Chart"}, []string{err.Error()}, []string{}, []string{})
 }
 
 // ErrConvertingAppVersionToChartVersion is the error for converting app version to chart version
 func ErrConvertingAppVersionToChartVersion(err error) error {
-	return errors.NewDefault(
-		ErrConvertingAppVersionToChartVersionCode,
-		fmt.Sprintf("error converting app version to chart version: %s", err.Error()),
-	)
+	return errors.New(ErrConvertingAppVersionToChartVersionCode, errors.Alert, []string{"Error occured while converting app version to chart version"}, []string{err.Error()}, []string{}, []string{})
 }
 
 // ErrCreatingNSMMeshClient is the error for streaming event
 func ErrCreatingNSMMeshClient(err error) error {
-	return errors.NewDefault(
-		ErrCreatingNSMMeshClientCode,
-		fmt.Sprintf("Unable to create a new nsm client %s", err.Error()),
-	)
+	return errors.New(ErrCreatingNSMMeshClientCode, errors.Alert, []string{"Unable to create a new NSM client"}, []string{err.Error()}, []string{}, []string{})
 }
 
 // ErrSampleApp is the error for streaming event
 func ErrSampleApp(err error) error {
-	return errors.NewDefault(ErrSampleAppCode, fmt.Sprintf("Error with sample app operation: %s", err.Error()))
+	return errors.New(ErrSampleAppCode, errors.Alert, []string{"Error with sample app operation"}, []string{err.Error()}, []string{}, []string{})
 }
 
 // ErrCustomOperation is the error for streaming event
 func ErrCustomOperation(err error) error {
-	return errors.NewDefault(ErrCustomOperationCode, fmt.Sprintf("Error with custom operation: %s", err.Error()))
+	return errors.New(ErrCustomOperationCode, errors.Alert, []string{"Error with custom operation"}, []string{err.Error()}, []string{}, []string{})
 }
